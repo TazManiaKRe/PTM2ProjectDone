871608b3a70b052901dfad0c14075f9de561458c
LianNidam <liannidam04@gmail.com>
2021-06-29 20:37:17 +0300

    anomaly report

--- src/Algos/AnomalyReport.java
+++ src/Algos/AnomalyReport.java
@@ -3,7 +3,8 @@ package Algos;
 public class AnomalyReport {
 	public final String des;
 	public final  long stenp;
-	
+
+
 	public AnomalyReport(String description, long timeStep){
 		this.des =description;
 		this.stenp =timeStep;
